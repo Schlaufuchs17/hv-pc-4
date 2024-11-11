cf9fadfd8517f9c43c46aa20157a4c957d26cef9
Adrian Caballero <92646371+Schlaufuchs17@users.noreply.github.com>
2024-11-11 20:45:11 +0100

    Arreglo de estilos css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import '../src/client/styles/style.css';  
+import '../src/client/styles/globals.css';
 
 function ChatNext({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
